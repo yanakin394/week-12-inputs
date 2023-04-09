976bfb3ea7d187f9f2920d9a9c4fd0ac5506dae3
yanakin394 <yana.germatskaya@gmail.com>
2023-04-09 23:29:28 +0300

    messages pop in chat

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-let nameInput = document.getElementById('name_input').textContent; //берем текст из инпута для имени
-console.log(nameInput);// проверка
+// берем данные кнопки
+const click = document.querySelector('.button'); 
 
-let nameInputCorrection = () => { // функция для удаления пробелов по сторонам
-    return nameInput.trim();
-}
-console.log(nameInputCorrection);// проверка
+// создаем обработчик событий
+click.addEventListener('click', () => {
+    //задаю переменные для полученных значений после ввода юзером
+    let userName = document.getElementById('name_input').value; 
+    let comment = document.getElementById('msg_input').value;
+    let avatar = document.getElementById('link_input').value;
+    
+    let userNameChecked = userName.trim(); // проверяем на пробелы
+    //функция проверки на спам НЕ РАБОТАЕТ
+    let commentChecked = () => {
+        let checkSpam = comment.replace(/viagra/gi, "***").replace(/xxx/gi, "***");
+        console.log(checkSpam);
+    } 
+    document.querySelector('.avatar').innerHTML = avatar; // функция для вывода аватара в чат ПОКА НЕ РАБОТАЕТ
+    document.querySelector('.username').innerText = userNameChecked; // функция для вывода проверенного юзернейма в чат
+    document.querySelector('.message').innerText = commentChecked; // функция для вывода  сообщения в чат
+})
